aa479edf035aa67c521964c5987fa452897b62db
Jiyoung Yun <t2wish@gmail.com>
2021-04-15 07:17:01 +0900

    Update insertionSort
    Signed-off-by: Jiyoung Yun <t2wish@gmail.com>

--- psr/insertionSort.cpp
+++ psr/insertionSort.cpp
@@ -8,16 +8,27 @@ int arr[MAX_DATA + 1];
 void insertionSort()
 {
   int t;
+  int j;
 
-  for (int i = 0; i < N; ++i) {
-    for (int j = i + 1; j < N; ++j) {
-      if (arr[i] > arr[j]) {
-        t = arr[i];
-        arr[i] = arr[j];
-        arr[j] = t;
-      }
+  for (int i = 1; i < N; ++i) {
+    t = arr[i];
+    j = i - 1;
+    while(t < arr[j] && j >= 0) {
+      arr[j + 1] = arr[j];
+      j -= 1;
     }
+    arr[j + 1] = t;
   }
+
+//  for (int i = 1; i < N; ++i) {
+//    for (int j = i + 1; j < N; ++j) {
+//      if (arr[i] > arr[j]) {
+//        t = arr[i];
+//        arr[i] = arr[j];
+//        arr[j] = t;
+//      }
+//    }
+//  }
 }
 
 int main()
